6d7507ec0bf11d465b7b663c1d91fe435cafda3b
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2024-11-17 21:26:52 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 100 ms, Memory: 9524 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -0,0 +1,8 @@
+const path = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = require('fs').readFileSync(path).toString().trim();
+
+const number = Number(input);
+
+for (let i = 1; i <= number; i++) {
+  console.log(' '.repeat(number - i) + '*'.repeat(i));
+}
