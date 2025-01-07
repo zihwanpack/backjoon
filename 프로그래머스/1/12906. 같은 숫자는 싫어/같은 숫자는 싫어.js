3ec04a968f31c6e5b8f17dcdfb19ed811234793f
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2025-01-07 17:36:41 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 45.49 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,12 @@
+function solution(arr)
+{
+    const stack = [];
+
+    arr.forEach(num => {
+        if (stack[stack.length - 1] !== num) {
+            stack.push(num)
+        };
+    })
+    
+    return stack;
+}
