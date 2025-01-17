e1a30a758137e58861b2f58d6286bf4c803f547c
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2025-01-17 23:13:08 +0900

    [level 2] Title: 주식가격, Time: 25.63 ms, Memory: 41.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,4 +1,19 @@
 function solution(prices) {
-    var answer = [];
+    const answer = Array(prices.length).fill(0);
+    const stack = [];
+
+    for (let i = 0; i < prices.length; i++) {
+        while (stack.length && prices[stack[stack.length - 1]] > prices[i]) {
+            const top = stack.pop();
+            answer[top] = i - top;
+        }
+        stack.push(i);
+    }
+
+    while (stack.length) {
+        const top = stack.pop();
+        answer[top] = prices.length - 1 - top;
+    }
+
     return answer;
 }
