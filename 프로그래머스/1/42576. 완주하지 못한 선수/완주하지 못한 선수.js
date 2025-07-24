32099a6766efdab623871710938674017b82f5b7
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2025-07-24 20:54:48 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 59.23 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,13 +1,24 @@
-function solution(participant, completion) {
-    const answer = [];
-    const sortedP = participant.sort();
-    const sortedC = completion.sort();
-    
-    for (let i = 0; i < sortedP.length; i++) {
-        if (sortedP[i] !== sortedC[i]) {
-            answer.push(sortedC[i]);
-        }
+function enter (map, participant) {
+    for (let i = 0; i < participant.length; i++) {
+        const man = participant[i];
+        map.set(man, (map.get(man) || 0) + 1);
     }
-    console.log(...answer);
-    // return answer;
+    return map;
+}
+
+function checking (participantMap, completion) {
+    for (let j = 0; j < completion.length; j++) {
+        const completionMan = completion[j];
+        participantMap.set(completionMan, participantMap.get(completionMan) - 1);
+    }
+    return participantMap;
+}
+
+function solution (participant, completion) {
+    const map = new Map();
+    const participantMap = enter(map, participant);
+    const checkedMap = checking(participantMap, completion);
+    for (let [man, isCompletion] of checkedMap) {
+        if (isCompletion > 0) return man;
+    }
 }
