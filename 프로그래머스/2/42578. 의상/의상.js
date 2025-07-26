898bcc9384bcd5574d1cf03b4c06e171bec16c9a
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2025-07-26 22:51:57 +0900

    [level 2] Title: 의상, Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,15 +1,20 @@
-function solution(clothes) {
-    let answer = 1;
-    const map = new Map();
-    
-    clothes.forEach(cloth => {
-        const [name, type] = cloth;
-        map.set(type, (map.get(type) || 0) + 1);
-    })
-    
-     map.forEach(count => {
-        answer *= (count + 1);
-    });
+function getClothes (clothes) {
+    const hash = new Map();
+    for (let [name, category] of clothes) {
+        hash.set(category, (hash.get(category) || 0) + 1);
+    }
+    return hash;
+}
 
+function countCombination (hash) {
+    let answer = 1;
+    for (let [category, count] of hash) {
+        answer *= (count + 1);
+    }
     return answer - 1;
+}
+
+function solution(clothes) {
+    const hash = getClothes(clothes);
+    return countCombination(hash);
 }
