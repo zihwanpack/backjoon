0731323a24e5ab3a3ffb3533b2a70b58333cf21c
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2025-07-26 17:31:03 +0900

    [level 2] Title: 전화번호 목록, Time: 192.78 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,10 +1,6 @@
 function solution(phone_book) {
-    let answer = true;
-    
+    var answer = true;
     const sortedBook = phone_book.sort();
-    for (let i = 0; i < sortedBook.length - 1; i++) {
-        if (sortedBook[i + 1].startsWith(sortedBook[i])) answer = false;
-    }
-    
+    console.log(sortedBook);
     return answer;
 }
