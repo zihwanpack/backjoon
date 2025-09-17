f8ca84860d62c08c2186997093f9d894c81a4d91
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2025-09-18 01:54:27 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+A, B, C = map(int, input().split())
+
+if A == B == C:
+    print(10000 + A * 1000)
+elif A == B or A == C:
+    print(1000 + A * 100)
+elif B == C:
+    print(1000 + B * 100)
+else:
+    print(max(A, B, C) * 100)
