df9d67723cb21b10f1c5701fb017ac7e71615d69
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2025-09-19 03:47:09 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
@@ -0,0 +1,10 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+
+for i in range(N):
+    print(" " * i + "*" * (2 * (N - i) - 1))
+
+for i in range(1, N):
+    print(" " * (N - i - 1) + "*" * (2 * i + 1))
