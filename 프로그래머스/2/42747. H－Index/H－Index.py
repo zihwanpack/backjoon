8974ff66864ddb53798c8a133e52b1cacad7ae73
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2026-04-06 12:38:19 +0900

    [level 2] Title: H-Index, Time: 0.24 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,8 @@
+def solution(citations):
+    answer = 0
+    citations.sort(reverse=True)
+    for i in range(len(citations)):
+        citation = citations[i]
+        if citation >= i + 1:
+            answer = i + 1
+    return answer
