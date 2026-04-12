d4ee52d7a2a2ecd61bd2145854718a1ad8e6329b
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2026-04-13 08:36:55 +0900

    [level 2] Title: 구명보트, Time: 8.73 ms, Memory: 9.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,15 @@
+def solution(people, limit):
+    answer = 0
+    left = 0
+    right = len(people) - 1
+    people.sort()
+    
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            answer += 1
+            right -= 1
+            left += 1
+        else:
+            right -= 1
+            answer += 1
+    return answer
