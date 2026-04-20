2397642ecbe28cff89dba026211334c90aaf23f2
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2026-04-21 08:46:13 +0900

    [level 2] Title: 더 맵게, Time: 1953.54 ms, Memory: 50.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,16 @@
+import heapq
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    
+    while scoville[0] < K:
+        if len(scoville) < 2:
+            return -1
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+        mixed_scoville = first + (second * 2)
+        heapq.heappush(scoville, mixed_scoville)
+        answer += 1
+            
+    return answer
