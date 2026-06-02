8b902e0ba2a0c08affa612e3c97d0557afb5c8ed
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2026-06-02 09:32:35 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 2813.48 ms, Memory: 44.7 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,12 +1,11 @@
 def solution(elements):
-    ## 원형 수열의 경우 * 2해서 처리해도 간으
-    extends_elements = elements * 2
+    answer = 0
+    elements_arr = elements * 2
     results = set()
     
-    for i in range(1, len(elements) + 1):
+    for num in range(1, len(elements) + 1):
         for start in range(len(elements)):
-            result = sum(extends_elements[start : start + i])
-            results.add(result)
+            sub_result = sum(elements_arr[start : start + num])
+            results.add(sub_result)
             
-    
     return len(results)
