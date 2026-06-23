b6a2d10efe4330f94d9c647f787cdb5003a8da66
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2026-06-23 17:47:08 +0900

    [level 2] Title: 방문 길이, Time: 0.23 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,23 +1,27 @@
-def is_valid_location(location):
-    if -5 > location[0] or 5 < location[0] or -5 > location[1] or 5 < location[1]:
+
+def is_valid_location(next_location):
+    x, y = next_location
+    if abs(x) > 5 or abs(y) > 5:
         return False
-    return True
+    return True    
 
 def solution(dirs):
     answer = 0
-    current_location = (0, 0)
     move = {'U': (0, 1), 'D': (0, -1), 'R': (1, 0), 'L': (-1, 0)}
-    dirs_memory = set()
+    # list 함수 사용해 배열로 변환
+    directions = list(dirs)
+    moving_record = set()
+    # 현재 위치 기록용 튜플
+    current_location = (0, 0)
     
-    for direction in dirs:
-        x, y = current_location
-        mx, my = move[direction]
-        next_location = (x + mx, y + my)
-                
+    for direction in directions:
+        cur_x, cur_y = current_location
+        move_x, move_y = move[direction]
+        next_location = (cur_x + move_x, cur_y + move_y)
+        
         if is_valid_location(next_location):
-            ## 길 기억하기
-            dirs_memory.add((current_location, next_location))
-            dirs_memory.add((next_location, current_location))
+            moving_record.add((current_location, next_location))
+            moving_record.add((next_location, current_location))
             current_location = next_location
-    
-    return len(dirs_memory) // 2
+            
+    return len(moving_record) // 2
