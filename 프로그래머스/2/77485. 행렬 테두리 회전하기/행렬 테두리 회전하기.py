e385cd53cf39f0d26d79aafefd8cb692467c7242
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2026-06-24 13:55:17 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 50.15 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
+++ 프로그래머스/2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.py
@@ -2,57 +2,47 @@ from collections import deque
 
 def solution(rows, columns, queries):
     answer = []
-   # 가로세로를 1씩 늘린 격자판 만들기
     matrix = [[0] * (columns + 1) for _ in range(rows + 1)]
-
-    # 1부터 시작하는 인덱스에 맞게 값 채우기
-    num = 1
-    for r in range(1, rows + 1):
-        for c in range(1, columns + 1):
-            matrix[r][c] = num
-            num += 1
+    number = 1
+    
+    for i in range(1, rows + 1):
+        for j in range(1, columns + 1):
+            matrix[i][j] = number
+            number += 1
     
     for query in queries:
-        r1, c1, r2, c2 = query[0], query[1], query[2], query[3]
-        temp_arr = []
-        # 위쪽 변 좌우로 진행
-        for c in range(c1, c2):
-            temp_arr.append(matrix[r1][c])
-        # 오른쪽 변 상하로 진행
-        for r in range(r1, r2):
-            temp_arr.append(matrix[r][c2])
-        # 아래쪽 변 우좌로 진행
-        for c in range(c2, c1, -1):
-            temp_arr.append(matrix[r2][c])
-        # 왼쪽 변 하상으로 진행
-        for r in range(r2, r1, -1):
-            temp_arr.append(matrix[r][c1])
+        queue = deque()
+        start_x, start_y, end_x, end_y = query
         
-        answer.append(min(temp_arr))
+        # 윗변 (좌 -> 우)
+        for my in range(start_y, end_y):
+            queue.append(matrix[start_x][my])
+        # 우변 (위 -> 아래)
+        for mx in range(start_x, end_x):
+            queue.append(matrix[mx][end_y])
+        # 밑변 (우 -> 좌)
+        for my in range(end_y, start_y, -1):
+            queue.append(matrix[end_x][my])
+        # 좌변 (아래 -> 위)
+        for mx in range(end_x, start_x, -1):
+            queue.append(matrix[mx][start_y])
         
-        curr_deck = deque(temp_arr)
-        curr_deck.rotate(1)
-            
-        idx = 0
+        answer.append(min(queue))
+        # 오른쪽으로 한번 회전
+        queue.rotate(1)
+        # 돌아간 값 넣기
+        # 윗변 (좌 -> 우)
+        for my in range(start_y, end_y):
+            matrix[start_x][my] = queue.popleft()
+        # 우변 (위 -> 아래)
+        for mx in range(start_x, end_x):
+            matrix[mx][end_y] = queue.popleft()
+        # 밑변 (우 -> 좌)
+        for my in range(end_y, start_y, -1):
+            matrix[end_x][my] = queue.popleft()            
+        # 좌변 (아래 -> 위)
+        for mx in range(end_x, start_x, -1):
+            matrix[mx][start_y] = queue.popleft()            
         
-        # 위쪽 변 다시 채우기
-        for c in range(c1, c2):
-            matrix[r1][c] = curr_deck[idx]
-            idx += 1
-            
-        # 오른쪽 변 다시 채우기
-        for r in range(r1, r2):
-            matrix[r][c2] = curr_deck[idx]
-            idx += 1
-            
-        # 아래쪽 변 다시 채우기
-        for c in range(c2, c1, -1):
-            matrix[r2][c] = curr_deck[idx]
-            idx += 1
-            
-        # 왼쪽 변 다시 채우기
-        for r in range(r2, r1, -1):
-            matrix[r][c1] = curr_deck[idx]
-            idx += 1
     
     return answer
