1f432b359899c4348aa48a6371a78218ddfa86a8
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2026-06-30 14:22:54 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.90 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,31 +1,31 @@
 from collections import deque
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def bfs(maps):
+def solution(maps):
+    answer = 0
     n = len(maps)
     m = len(maps[0])
+    
     queue = deque()
     queue.append((0, 0))
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
     
     while queue:
-        x, y = queue.popleft()
+        current_x, current_y = queue.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            moving_x = dx[i] + current_x
+            moving_y = dy[i] + current_y
+            # 게임 맵 벗어나는지 체크
+            if moving_x < 0 or moving_x >= n or moving_y < 0 or moving_y >= m:
                 continue
-            if maps[nx][ny] == 1:
-                maps[nx][ny] = maps[x][y] + 1
-                queue.append((nx, ny))
-    if maps[n - 1][m - 1] == 1:
-            return -1
-    return maps[n - 1][m - 1]
-            
-                
-        
+            # 갈 수 있는 길 체크
+            if maps[moving_x][moving_y] == 1:
+                # 거리로 값 써버리기
+                maps[moving_x][moving_y] = maps[current_x][current_y] + 1
+                queue.append((moving_x, moving_y))
+    if maps[n-1][m-1] == 1:
+        return -1
 
-def solution(maps):
-    answer = bfs(maps)
-    return answer
+    return maps[n-1][m-1]
+        
