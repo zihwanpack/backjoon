8e8dd323560872e17c83400b73e7225d3835ea9e
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2026-07-01 09:15:56 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,18 +1,16 @@
-from collections import deque
-
 def solution(n, computers):
-    queue = deque()
     answer = 0
     visited = [False] * n
+    
+    def dfs(computer_index):
+        visited[computer_index] = True
+        
+        for i in range(n):
+            if computers[computer_index][i] == 1 and not visited[i]:
+                dfs(i)
+                
     for i in range(n):
         if not visited[i]:
+            dfs(i)
             answer += 1
-            queue.append(i)
-            visited[i] = True
-            while queue:
-                curComputer = queue.popleft()
-                for j in range(n):
-                    if computers[curComputer][j] == 1 and not visited[j]:
-                        visited[j] = True
-                        queue.append(j)
     return answer
