a8990fd9a2c1e874365475873f3dbcb564a94760
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2026-07-03 16:02:07 +0900

    [level 2] Title: 소수 찾기, Time: 5.57 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,30 +1,38 @@
-import math
-from itertools import permutations
+from math import isqrt
 
 def is_prime(number):
     if number < 2:
         return False
-    for i in range(2, int(math.sqrt(number) + 1)):
-        if number % i == 0:
+    for i in range(2, isqrt(number) + 1):
+        if not number % i:
             return False
     return True
-def get_all_numbers(string_numbers):
-    every_numbers = set()
-    for i in range(1, len(string_numbers) + 1):
-        tuples = permutations(string_numbers, i)
-        for permutation in tuples:
-            number = int("".join(permutation))
-            every_numbers.add(number)
-    return every_numbers
-        
 
+def solution(numbers):
+    n = len(numbers)
+    visited = [False] * n
+    # 소수의 중복을 허락하면 안되니까 집합 사용
+    prime_set = set()
+    numbers = list(numbers)
     
-def solution(string_numbers):
-    answer = 0
-    parsed_numbers = get_all_numbers(string_numbers)
+    def backtrack(number_str):
+        # 빈문자열이 아닌 경우
+        if number_str:
+            # 숫자로 형변환
+            number = int(number_str)
+            # 소수 검사
+            if is_prime(number):
+                prime_set.add(number)
+        # 한번 조합으로 가능한 모든 수 도달 시 돌아가기
+        if len(number_str) == n:
+            return
+        
+        for i in range(n):
+            if not visited[i]:
+                visited[i] = True
+                backtrack(number_str + numbers[i])
+                visited[i] = False
+    backtrack('')
     
-    for n in parsed_numbers:
-        if is_prime(n):
-            answer += 1
-    
-    return answer
+    return len(prime_set)
+                
