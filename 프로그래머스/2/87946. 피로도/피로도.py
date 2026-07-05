35eb2e69f922ec263ce5187d8e60c48c1a18183e
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2026-07-06 00:09:27 +0900

    [level 2] Title: 피로도, Time: 18.98 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,15 +1,22 @@
-from itertools import permutations
+def backtracking(answer, k, count, visited, dungeons):
+    # answer를 계속 갱신해주기
+    answer = max(answer, count)
+    n = len(dungeons)
+    
+    for i in range(n):
+        limit, cost = dungeons[i]
+        # 방문하지 않았고 k가 limit보다 크거나 같으면 count 처리
+        if not visited[i] and limit <= k:
+            visited[i] = True
+            answer = backtracking(answer, k - cost, count + 1, visited, dungeons)
+            visited[i] = False
+    return answer
 def solution(k, dungeons):
     answer = 0
+    n = len(dungeons)
+    visited = [False] * n
+    count = 0
     
-    for p in permutations(dungeons):
-        count = 0
-        current_health = k
-        for limit, damage in p:
-            if limit <= current_health:
-                current_health -= damage
-                count += 1
-            else:
-                break
-        answer = max(count, answer)
+    answer = backtracking(answer, k, count, visited, dungeons)
+    
     return answer
