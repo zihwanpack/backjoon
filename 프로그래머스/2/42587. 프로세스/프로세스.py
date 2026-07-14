c3bf442ba470374cbc5d259f0323af871eb38986
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2026-07-14 16:28:02 +0900

    [level 2] Title: 프로세스, Time: 0.22 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -2,20 +2,21 @@ from collections import deque
 
 def solution(priorities, location):
     answer = 0
-    queue = deque()
-    # queue 만들기
-    for index, priority in enumerate(priorities):
-        queue.append((index, priority))
+    queue = deque([(index, priority) for index, priority in enumerate(priorities)])
     
     while queue:
-        target_index, tartget_priority = queue.popleft()
-        highest_priority = max(q[1] for q in queue) if queue else 0
+        index, current_priority = queue.popleft()
         
-        if highest_priority > tartget_priority:
-            queue.append((target_index, tartget_priority))
+        has_higher_priority = False
         
+        for process in queue:
+            if process[1] > current_priority:
+                has_higher_priority = True
+                break 
+        
+        if has_higher_priority:
+            queue.append((index, current_priority))
         else:
             answer += 1
-            if location == target_index:
+            if index == location:
                 return answer
-        
