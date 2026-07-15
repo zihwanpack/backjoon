cf62402a078c7d013fdf86d87ae17a83290ab412
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2026-07-15 11:57:28 +0900

    [level 2] Title: 주식가격, Time: 14.71 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,16 +1,3 @@
-# 단조스택 풀이
 def solution(prices):
-    N = len(prices)
-    answer = [0] * N
-    stack = []
-    
-    for index in range(N):
-        price = prices[index]
-        while stack and prices[stack[-1]] > price:
-            past_index = stack.pop()
-            answer[past_index] = index - past_index
-        stack.append(index)
-    while stack:
-        past_index = stack.pop()
-        answer[past_index] = N - 1 - past_index
+    answer = []
     return answer
