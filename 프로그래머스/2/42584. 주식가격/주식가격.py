34c1c916e798de145fdbbe57786f7c1ccf5facb0
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2026-07-15 11:57:07 +0900

    [level 2] Title: 주식가격, Time: 14.71 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,15 +1,16 @@
-from collections import deque
-
+# 단조스택 풀이
 def solution(prices):
-    answer = []
-    queue = deque(prices)
+    N = len(prices)
+    answer = [0] * N
+    stack = []
     
-    while queue:
-        time = 0
-        cost = queue.popleft()
-        for after_price in queue:
-            time += 1
-            if after_price < cost:
-                break
-        answer.append(time)
+    for index in range(N):
+        price = prices[index]
+        while stack and prices[stack[-1]] > price:
+            past_index = stack.pop()
+            answer[past_index] = index - past_index
+        stack.append(index)
+    while stack:
+        past_index = stack.pop()
+        answer[past_index] = N - 1 - past_index
     return answer
